7f9a07c9f627886b4902270421fa04a88113aed0
Lam Nguyễn Mai <nguyenlamit86@gmail.com>
2016-11-01 21:24:29 +0700

    ABC

---
+++ src/main/java/com/greenlucky/DevopsglApplication.java
@@ -0,0 +1,12 @@
+package com.greenlucky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsglApplication.class, args);
+	}
+}
---
+++ src/main/java/com/greenlucky/web/DevopsglApplication.java
@@ -0,0 +1,12 @@
+package com.greenlucky.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsglApplication.class, args);
+	}
+}
