af027e93e5355b951835bd44b9a254a2014ae5c0
Lam Nguyễn Mai <nguyenlamit86@gmail.com>
2016-11-03 22:28:52 +0700

    Add logging setup with logback

--- src/main/java/com/greenlucky/web/controllers/IndexController.java
+++ src/main/java/com/greenlucky/web/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package com.greenlucky.web.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +11,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class IndexController {
+
+    /** The application logger */
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+
 
     @RequestMapping("/")
     public String home(){
--- src/test/java/com/DevopsglApplicationTests.java
+++ src/test/java/com/DevopsglApplicationTests.java
@@ -1,16 +1,29 @@
 package com;
 
+import com.greenlucky.DevopsglApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DevopsglApplication.class)
 @SpringBootTest
 public class DevopsglApplicationTests {
 
+	/** The application logger */
+	private static final Logger LOGGER = LoggerFactory.getLogger(DevopsglApplicationTests.class);
+
+
 	@Test
 	public void contextLoads() {
+		LOGGER.debug("Message test Logger..");
 	}
 
 }
